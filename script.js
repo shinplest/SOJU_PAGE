90fc189baf675214a8ce8ddd79e68cd68028bcad
Jaihyoung Shin <shineceo97@gmail.com>
2019-05-06 18:50:20 +0900

    separate_file

---
+++ script.js
@@ -0,0 +1,10 @@
+var age = prompt("만 19세 이상만 이 페이지를 볼 수 있습니다. 생년월일을 입력하세요 ", "19970701");
+    if(age > 19970101)
+    {
+        alert("you cant");
+
+    }
+    else
+    {
+        alert(age);
+    }
