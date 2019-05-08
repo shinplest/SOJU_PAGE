ba41ba633ec8a49b55dab81041313cf49017d514
SuyeonJung09 <zyzy1231@naver.com>
2019-05-08 19:27:33 +0900

    오른쪽 네비 만드는 중

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var age = prompt("만 19세 이상만 이 페이지를 볼 수 있습니다. 생년월일을 입력하세요 ", "19970701");
-    if(age > 19970101)
+    if(age > 20000101)
     {
         alert("you cant");
 
@@ -8,3 +8,14 @@ var age = prompt("만 19세 이상만 이 페이지를 볼 수 있습니다. 생
     {
         alert(age);
     }
+
+function mover(){
+    var M;
+    M= document.getElementById("detail_original_soju");
+    M.style.display = 'block';
+}
+function mount(){
+    var M;
+    M= document.getElementById("detail_original_soju");
+    M.style.display = 'none';
+}
