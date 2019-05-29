50f7a660a8649bcc1830296864952600cb0aecce
Jaihyoung Shin <shineceo97@gmail.com>
2019-05-30 01:06:37 +0900

    commit

---
+++ svgjavascript.js
@@ -0,0 +1,74 @@
+//svg showing funtion
+function show(resign){
+  switch(resign)
+  {
+    case seoul:
+        document.getElementById("seoul").style.display = "inline";
+        break;
+    case gangwon:
+        document.getElementById("gangwon").style.display = "inline";
+        break;
+    case choongnam:
+        document.getElementById("choongnam").style.display = "inline";
+        break;
+    case choongbook:
+        document.getElementById("choongbook").style.display = "inline";
+        break;
+    case kyoungbook:
+        document.getElementById("kyoungbook").style.display = "inline";
+        break;
+    case jeounbook:
+        document.getElementById("jeounbook").style.display = "inline";
+        break;
+    case kyoungnam:
+        document.getElementById("kyoungnam").style.display = "inline";
+        break;
+    case jeounnam:
+        document.getElementById("jeounnam").style.display = "inline";
+        break;
+    case jeju:
+        document.getElementById("jeju").style.display = "inline";
+        break;
+    case busan:
+        document.getElementById("busan").style.display = "inline";
+        break;
+    
+  }
+}
+function hide(resign){
+  switch(resign)
+  {
+    case seoul:
+        document.getElementById("seoul").style.display = "none";
+        break;
+    case gangwon:
+        document.getElementById("gangwon").style.display = "none";
+        break;
+    case choongnam:
+        document.getElementById("choongnam").style.display = "none";
+        break;
+    case choongbook:
+        document.getElementById("choongbook").style.display = "none";
+        break;
+    case kyoungbook:
+        document.getElementById("kyoungbook").style.display = "none";
+        break;
+    case jeounbook:
+        document.getElementById("jeounbook").style.display = "none";
+        break;
+    case kyoungnam:
+        document.getElementById("kyoungnam").style.display = "none";
+        break;
+    case jeounnam:
+        document.getElementById("jeounnam").style.display = "none";
+        break;
+    case jeju:
+        document.getElementById("jeju").style.display = "none";
+        break;
+    case busan:
+          document.getElementById("busan").style.display = "none";
+          break;
+    
+  }
+}
+
